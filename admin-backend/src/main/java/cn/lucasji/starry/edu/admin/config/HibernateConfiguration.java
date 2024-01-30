7f1cdf401505bd2cc51cfb07e3aab8035f5b1f31
lucas <ymfs.l.j@outlook.com>
2024-01-30 11:12:20 +0800

    Fix conflicts between hibernate and openfeign jackson configuration

--- admin-backend/src/main/java/cn/lucasji/starry/edu/admin/config/HibernateConfiguration.java
+++ admin-backend/src/main/java/cn/lucasji/starry/edu/admin/config/HibernateConfiguration.java
@@ -3,12 +3,13 @@ package cn.lucasji.starry.edu.admin.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module.Feature;
-import java.io.Serial;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.type.format.jackson.JacksonJsonFormatMapper;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.Serial;
 
 /**
  * @author lucas
@@ -17,23 +18,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HibernateConfiguration {
 
-  @Bean
   public HibernateAwareObjectMapper hibernateAwareObjectMapper() {
     return new HibernateAwareObjectMapper();
   }
 
   @Bean
-  public HibernatePropertiesCustomizer hibernatePropertiesCustomizer(
-      HibernateAwareObjectMapper objectMapper) {
+  public HibernatePropertiesCustomizer hibernatePropertiesCustomizer() {
     return hibernateProperties -> {
       hibernateProperties.put(
-          AvailableSettings.JSON_FORMAT_MAPPER, new JacksonJsonFormatMapper(objectMapper));
+        AvailableSettings.JSON_FORMAT_MAPPER,
+        new JacksonJsonFormatMapper(hibernateAwareObjectMapper()));
     };
   }
 
   public static class HibernateAwareObjectMapper extends ObjectMapper {
 
-    @Serial private static final long serialVersionUID = -8104466180562970076L;
+    @Serial
+    private static final long serialVersionUID = -8104466180562970076L;
 
     public HibernateAwareObjectMapper() {
       Hibernate6Module hibernate6Module = new Hibernate6Module();
