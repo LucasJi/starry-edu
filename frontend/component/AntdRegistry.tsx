9683cd4d64e19972f51e5609cee54fd63bd72e45
lucas <ymfs.l.j@outlook.com>
2024-03-07 15:10:25 +0800

    Fix client side error

--- frontend/component/AntdRegistry.tsx
+++ frontend/component/AntdRegistry.tsx
@@ -1,18 +1,12 @@
 'use client';
 
-import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
+import { StyleProvider } from '@ant-design/cssinjs';
 import { useServerInsertedHTML } from 'next/navigation.js';
 import { ReactNode } from 'react';
 
 const StyledComponentsRegistry = ({ children }: { children: ReactNode }) => {
-  const cache = createCache();
-  useServerInsertedHTML(() => (
-    <style
-      dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }}
-      id="antd"
-    />
-  ));
-  return <StyleProvider cache={cache}>{children}</StyleProvider>;
+  useServerInsertedHTML(() => <style id="antd" />);
+  return <StyleProvider>{children}</StyleProvider>;
 };
 
 export default StyledComponentsRegistry;
